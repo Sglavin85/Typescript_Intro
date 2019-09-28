2c772c4dbc20a9a34d9f921d6cbd5b9361ea6c62
Sean Glavin <sean@glavin.us>
2019-09-28 10:49:12 -0500

    inital branch commit

--- dist/typescript.e6746b00.js
+++ dist/typescript.e6746b00.js
@@ -134,7 +134,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '59141' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60891' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
